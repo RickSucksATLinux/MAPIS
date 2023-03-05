b0ef695dd6b6154f804a8c4b577611fcffc8aeee
RickSucksATLinux <124423658+RickSucksATLinux@users.noreply.github.com>
2023-03-05 17:33:41 +0630

    Update launch-in-usb.sh

--- launch-in-usb.sh
+++ launch-in-usb.sh
@@ -4,11 +4,11 @@
 usb_location_point="/run/media/richard/Ventoy"
 
 # Copy the files from the USB to your local system
-cp "$usb_mount_point/MAPIS" "$HOME/"
+cp "$usb_location_point/MAPIS" "$HOME/"
  
 # Run the scripts
 "$HOME/MAPIS/install.sh"
 "$HOME/MAPIS/i3wmconfig.sh"
 
 # Unmount the USB drive
-sudo umount "$usb_mount_point"
+sudo umount "$usb_location_point"
